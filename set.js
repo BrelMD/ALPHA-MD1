327fcc215a2545beeb171ce4bfdad140cef24829
BrelMD <soulblackblk@gmail.com>
2024-10-06 18:45:03 +0100

    Update set.js

--- set.js
+++ set.js
@@ -9,9 +9,9 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "keithkeizzah",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " keithkeizzah",              
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
+    OWNER_NAME: process.env.OWNER_NAME || "💀ᡃ⃢⃟🇨🇬𝘾𝙊𝙉𝙁𝙐𝘾𝙄𝙐𝙎🇨🇬ᡃ⃢⃟⃢💀)",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "242055617560",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "no",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || '𝐀𝐋𝐏𝐇𝐀-𝐌𝐃',
     URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/0c351a67f1dffd1f34cf5.jpg',
@@ -20,13 +20,13 @@ module.exports = { session: process.env.SESSION_ID || '',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
     HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
-    ETAT : process.env.PRESENCE || '',
+    ETAT : process.env.PRESENCE || '4',
     CHATBOT : process.env.PM_CHATBOT || 'no',
     DP : process.env.STARTING_BOT_MESSAGE || "yes",
     ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+        ? "postgresql://brel_database_user:GIURl9Eo4XmK2qAIwYM4JfS0LJ4Tp6cn@dpg-crdo0sggph6c73ehu2eg-a.oregon-postgres.render.com/brel_database" : "postgresql://brel_database_user:GIURl9Eo4XmK2qAIwYM4JfS0LJ4Tp6cn@dpg-crdo0sggph6c73ehu2eg-a.oregon-postgres.render.com/brel_database",
    
 };
 let fichier = require.resolve(__filename);
